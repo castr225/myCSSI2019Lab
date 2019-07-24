2146ac9b53fa3cad2af5d5c52cf129a618e9ba23
MarcJoseph Castro <castr225@csusm.edu>
2019-07-24 12:00:14 -0700

    javascript inputs and outputs to html and css

---
+++ WeLearn/M2-javaScript/L1-JSIntro/script.js
@@ -0,0 +1,12 @@
+console.log("script running!");
+
+let name = 'Charlie';
+console.log('Hello' + name +'!');
+let age = 15;
+console.log('You are' + age + 'years old!');
+
+if (age >= 16) {
+  console.log('I can drive!')
+} else {
+  console.log('sucks to suck.')
+}
